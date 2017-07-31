760e91398ed013db63dc99832eda5eb5f6d32d96
swt <wentao.sun@rakwireless.com>
2017-07-31 17:41:11 +0800

    support kernel download

--- wiswrt/rak/purewrt-rc2/scripts/download.pl
+++ wiswrt/rak/purewrt-rc2/scripts/download.pl
@@ -179,6 +179,7 @@ foreach my $mirror (@ARGV) {
 		foreach my $dir (@extra) {
 			push @mirrors, "ftp://ftp.all.kernel.org/pub/$dir";
 			push @mirrors, "http://ftp.all.kernel.org/pub/$dir";
+
 			push @mirrors, "https://kernel.org/pub/$dir"; 
 			push @mirrors, "ftp://kernel.org/pub/$dir";
 		}
